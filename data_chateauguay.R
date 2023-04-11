7249459d4cbfa1298abe97016038b8cbee7a1f6c
Laurie Boulerice <laurie.boulerice@usherbrooke.ca>
2023-04-11 15:06:05 -0400

    only chateauguay data

--- data_chateauguay.R
+++ data_chateauguay.R
@@ -6,6 +6,8 @@ library(stringr)
 library(dplyr)
 library(janitor)
 library(rgdal)
+library(ade4)
+library(vegan)
 
 ############
 #eDNA data
@@ -27,73 +29,54 @@ chatauguay_sf <- st_as_sf(x =chatauguay_env,
 
 mapview(chatauguay_sf)
 
-#open saint_francois data
-saint_francois_data<-read_excel('Résultat_ADNe_Saint-François_2021_.xlsx',sheet='présence_absence')
-saint_francois_df<-as.data.frame(saint_francois_data)
-
-#subset for only environmental data with coordinates
-saint_francois_env<-saint_francois_df[c(3:146),c(1:9)]
-
-#make it a spatial object
-saint_francois_sf <- st_as_sf(x =saint_francois_env, 
+#build community matrix 
+##make spatial object
+chatau_comm_sf <- st_as_sf(x =chatauguay_df[c(3:60),c(2,3,10:50)], 
                           coords = c("Longitude","Latitude"),
                           crs = "+proj=longlat +ellps=WGS84 +no_defs")
 
-mapview(saint_francois_sf)+chatauguay_sf
+#calculate Jaccards Index of similarity
+chatau_comm<-st_drop_geometry(chatau_comm_sf)
+
+chatau_vec<-as.vector(chatau_comm)
+
+mat_chatau<-do.call(cbind,chatau_vec)
+
+class(mat_chatau)<-'numeric'
+
+chatau_jacc<-vegdist(mat_chatau,'jac',binary = TRUE)
+jacc_mat<-as.matrix(chatau_jacc)
+plot(rowSums(jacc_mat))
+
+chatau_jacc
+
+df = mat_chatau
+a <- df %*% t(df)
+b <- df %*% (1 - t(df))
+c <- (1 - df) %*% t(df)
+d <- ncol(df) - a - b - c
+
+simi = a / (a+b+c)
+
+diag(simi) = 0
+
+plot(rowSums(simi))
+
+col_chatau<-rowSums(simi)
+chatau_sim<-cbind(chatauguay_sf,col_chatau)
+mapview(chatau_sim,zcol='col_chatau')
+
+
 
 ##########
-#dam data
-#########
+#ennironmental data
+##########
 
-setwd('C:/Users/greco/OneDrive - USherbrooke/Maitrise/Projet de maitrise/data/environment')
-
-dam_raw <-
-  read_excel(
-    'C:/Users/greco/OneDrive - USherbrooke/Maitrise/Projet de maitrise/data/environment/repertoire_des_barrages.xls'
-  )
-
-#Set first row as column names
-dam<-janitor::row_to_names(dam_raw, 1, remove_rows_above = FALSE) 
-dam<-as.data.frame(dam) #turn into df
-
-#remove NA from coords columns
-dam_na<-dam[!is.na(dam$`Latitude (NAD 83)`),]
-# sum(is.na(dam_na$`Latitude (NAD 83)`)) #it worked
-
-#make it a spatial object
-dam_sf<- st_as_sf(x =dam_na, 
-                          coords = c("Longitude (NAD 83)","Latitude (NAD 83)"),
-                          crs = "+init=epsg:4269 +proj=longlat +ellps=GRS80 +datum=NAD83
-+no_defs +towgs84=0,0,0
-") 
-
-#mapview(dam_sf)
+#saint-francois
+env_fr<-saint_francois_env[,-c(2,3)]
 
 
-#mapview(saint_francois_sf,col.regions='red')+dam_sf
 
-#hydrography
-##for saint-francois river
-hydro_stf<-readOGR('C:/Users/greco/OneDrive - USherbrooke/Maitrise/Projet de maitrise/data/environment/hydro/GRHQ_03AF_GRP.gdb')
 
-##for chateauguay river
-hydro_chat<-readOGR('C:/Users/greco/OneDrive - USherbrooke/Maitrise/Projet de maitrise/data/environment/hydro/GRHQ_03AB.gdb')
 
-mapview(list(hydro_stf,saint_francois_sf,dam_zone),col.regions=list('blue','red','green'))
-   
-#unites de decoupage
-hydro_cut<-st_read('C:/Users/greco/OneDrive - USherbrooke/Maitrise/Projet de maitrise/data/environment/decoupage')
-hydro_03<-hydro_cut[hydro_cut$Bloc=='03',]
 
-#mapview(hydro_03)
-
-#cut dams out of the hydro zone
-##check projection
-st_crs(dam_sf) #not the same so transform to WGS84
-st_crs(hydro_03)
-
-dam_crs<-st_transform(dam_sf,4326)
-hydro_03_crs<-st_transform(hydro_03,4326)
-
-dam_zone<-st_intersection(dam_crs,hydro_03_crs)
-#mapview(dam_zone) #it works
