56d59dee0322479be11608359d1bc0766aed9e0c
Laurie Boulerice <laurie.boulerice@usherbrooke.ca>
2023-05-23 16:41:02 -0400

    variation partitioning chatauguay

--- analysis.R
+++ analysis.R
@@ -21,6 +21,7 @@ library(devtools)
 # and load it
 library(HMSC)
 library(fastDummies)
+library(ggtern)
 
 ###########
 #Chatauguay
@@ -39,25 +40,89 @@ space_chat<-readRDS("C:/Users/greco/OneDrive - USherbrooke/Maitrise/Projet de ma
 
 #format data
 mat_chatau<-data.frame(env_chat,space_chat)
+mat_chatau$Habitat[9]<-'Eau calme' #########!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!####### 
+
 ##make dummy variables of categorical data
 mat_chatau_env <- dummy_cols(mat_chatau, select_columns = c('Habitat', 'Plantes'),
-                      remove_selected_columns = TRUE)
-mat_chatau_env$O_Strahler<-as.numeric(as.character(mat_chatau_env$O_Strahler))
+                      remove_selected_columns = TRUE,remove_first_dummy = TRUE)
+mat_chatau_env<-mat_chatau_env[,-1]
+mat_chatau_env<-mat_chatau_env[,-12]
 
-mat_chatau_env<-mat_chatau_env[,-8] #remove NA
+chatau_env_hmsc<-mat_chatau_env[,c(10:14)]
+chatau_space_hmsc<-mat_chatau_env[,1:9]
 
-chatau_random<-as.factor(1:nrow(chatau_comm))
+chatau_random<-as.factor(1:nrow(chatau_comm_num))
 
 formdata_chatau <- as.HMSCdata(Y = as.matrix(chatau_comm_num),
                             X = mat_chatau_env,
                             Random = chatau_random,
-                            interceptX = TRUE,
+                            interceptX = FALSE,
                             scaleX = TRUE)
 
-model_chatau <- hmsc(formdata_chatau, family = "probit", niter = 99999, nburn = 30000)
-R2chatau<-Rsquared(model_chatau) ##0,709 #good
+# Estimate model parameters
+model_chatau <- hmsc(formdata_chatau, family = "probit", niter = 100000, nburn = 30000)
+R2chatau<-Rsquared(model_chatau,type='nakagawa') 
 
-mcmcParamX <- as.mcmc(model_chatau, parameters = "paramX")
-plot(mcmcParamX)
+# Check convergence
+mcmcParamX_chatau <- as.mcmc(model_chatau, parameters = "paramX")
+plot(mcmcParamX_chatau)
 
+effetSize_chatau <- effectiveSize(mcmcParamX_chatau)
+
+#======================
+#Variation partitioning 
+#======================
+
+#---------------
+##For chatauguay
+#---------------
+#Partition
+
+vari_chatau<-variPart(hmsc=model_chatau,groupX=c(rep('chatau_env_hmsc',5),rep('chatau_space_hmsc',9)),indSite=TRUE,R2adjust=TRUE,type='III')
+#saveRDS(vari_chatau,file="variation_part_chatauguay.RDS")
+
+##Organise data
+variFinal_chatau <- vari_chatau$overlap1
+df_variFinal_chatau<-as.data.frame(variFinal_chatau)
+# Environment
+variFinal_chatau[,,"chatau_env_hmsc"] <- vari_chatau$overlap1[,,"chatau_env_hmsc"] +
+  vari_chatau$overlap2[,,"chatau_env_hmsc-random"] +
+  vari_chatau$overlap2[,,"chatau_env_hmsc-chatau_space_hmsc"]/2 +
+  vari_chatau$overlap3/2
+
+# Space
+variFinal_chatau[,,"chatau_space_hmsc"] <- vari_chatau$overlap1[,,"chatau_space_hmsc"] +
+  vari_chatau$overlap2[,,"chatau_space_hmsc-random"] +
+  vari_chatau$overlap2[,,"chatau_env_hmsc-chatau_space_hmsc"]/2 +
+  vari_chatau$overlap3/2
+
+# Summary for plotting
+vp_chatau_Site <- apply(variFinal_chatau, c(1,3), mean)
+vp_chatau_Site<-ifelse(vp_chatau_Site<0,0,vp_chatau_Site)
+
+vp_chatau_Sp <- apply(variFinal_chatau, c(2,3), sum)
+vp_chatau_Sp<-ifelse(vp_chatau_Sp<0,0,vp_chatau_Sp)
+
+
+##Plot results
+
+###For sites
+par(mfrow = c(1, 2))
+ggtern(data=as.data.frame(vp_chatau_site_clust),
+       aes(random, chatau_space_hmsc, chatau_env_hmsc)) +
+  ggtitle("Sites rivière Châtauguay") +
+  geom_point(colour = rgb(0, 0, 0, 0.5),
+             size = rowSums(vp_chatau_site_clust[,-4])* 200,show.legend=TRUE) +
+  xlab("Co-Distribution") + ylab("Spatial") + zlab("Environnement")+
+  theme_rgbw()
+
+#for species
+par(mfrow = c(2, 2))
+ggtern(data=as.data.frame(vp_chatau_Sp),
+       aes(random, chatau_space_hmsc, chatau_env_hmsc)) +
+  ggtitle("Sp rivière Châtauguay") +
+  geom_point(colour = rgb(0, 0, 0, 0.5),
+             size = rowSums(vp_chatau_Sp)* 10,show.legend=TRUE) +
+  xlab("Co-Distribution") + ylab("Spatial") + zlab("Environnement")+
+  theme_rgbw()
 
